0219f3384275c474b24c2a9f3b701cf92afff958
combesf <florence.combes@cea.fr>
2017-09-18 14:18:21 +0200

    additional functions

--- R/bioAnalysis.R
+++ R/bioAnalysis.R
@@ -27,6 +27,75 @@ getUniprotID <- function(data){
 }
 
 
+
+##' Returns a vector of ENTREZ ID (gene id)  
+##' 
+##' @title Returns a vector of ENTREZ ID (gene id) 
+##' @param dat a vector of ID
+##' @param idFrom ID type of data to convert
+##' @param idTo ID to convert to
+##' @param orgdb a Bioconductor annotation package 
+##' @return A vector of ID of the asked type 
+##' @author Florence Combes
+##' @examples
+##' require(org.Hs.eg.db)
+##' dat<-c("P15924","P02538", "P02768", "P08779", "Q02413", "P14923")
+##' ID_conversion(dat, idFrom="UNIPROT", idTo="ENTREZID", orgdb=org.Hs.eg.db)
+ID_conversion<-function(dat, idFrom, idTo=ENTREZID, orgdb) {
+    
+    #require(clusterProfiler)
+    gene <- bitr(dat, fromType=idFrom, toType=idTo, OrgDb=orgdb)
+    gene.id = gene$ENTREZID
+    return(gene.id)
+}
+
+
+
+##' This function is a wrappper to the function groupGO from the
+##' package clusterProfiler. Given a vector of genes/proteins, it returns the 
+##' GO profile at a specific level. 
+##' 
+##' It returns a groupGOResult instance. 
+##' 
+##' 
+##' @title Calculates the GO profile of a vector of genes/proteins at specific 
+##' level
+##' @param data A vector of ID (genes or proteins !!DIRE LESQUELS!!)
+##' @param idFrom
+##' @param threshold_LogFC The threshold on log(Fold Change) to
+##' distinguish between differential and non-differential data 
+##' @param pi0Method The parameter pi0.method of the method adjust.p 
+##' in the package \code{cp4p}
+##' @return The computed FDR value (floating number)
+##' @author Florence Combes
+##' @examples
+##' require(DAPARdata)
+##' data(Exp1_R25_pept)
+##' obj <- wrapper.mvImputation(Exp1_R25_pept[1:1000], "QRILC")
+##' condition1 <- '25fmol'
+##' condition2 <- '10fmol'
+##' qData <- Biobase::exprs(obj)
+##' samplesData <- Biobase::pData(obj)
+##' labels <- Biobase::pData(obj)[,"Label"]
+##' limma <- diffAnaLimma(qData,samplesData, labels, condition1, condition2)
+##' diffAnaComputeFDR(limma)
+
+
+group_GO <- function(data, idFrom, idTo, orgdb, ont, level, readable=TRUE){
+    
+    require(as.character(orgdb),character.only = TRUE)
+    gene <- bitr(data, fromType=idFrom, toType=idTo, OrgDb=orgdb)
+    gene.id = gene$ENTREZID
+    ggo <- groupGO(gene = gene.id, 
+                 OrgDb = orgdb, 
+                 ont = ont, 
+                 level = level, 
+                 readable= TRUE)
+    
+    return(ggo)
+}
+
+
 ##' This function is a wrappper to the function groupGO from the
 ##' package clusterProfiler. Given a vector of genes/proteins, it returns the 
 ##' GO profile at a specific level. 
@@ -58,23 +127,6 @@ getUniprotID <- function(data){
 ##' diffAnaComputeFDR(limma)
 
 
-group_GO <- function(data, idFrom, idTo, orgdb, ont, level, readable=TRUE){
-    
-    require(as.character(orgdb),character.only = TRUE)
-    gene <- bitr(data, fromType=idFrom, toType=idTo, OrgDb=orgdb)
-    gene.id = gene$ENTREZID
-    ggo <- groupGO(gene = gene.id, 
-                 OrgDb = orgdb, 
-                 ont = ont, 
-                 level = level, 
-                 readable= TRUE)
-    
-    return(ggo)
-}
-
-
-
-
 enrich_GO <- function(data, idFrom, idTo, orgdb, ont, readable=TRUE, pAdj, pval, universe)
 {
     
@@ -91,9 +143,6 @@ enrich_GO <- function(data, idFrom, idTo, orgdb, ont, readable=TRUE, pAdj, pval,
     
     return(ego)
 }
-
-
-
 
 
 ###################################
@@ -116,4 +165,26 @@ getUniprotID_FromVector <- function(dat){
     
     return(unlist(uniprotID))
 }
+
+
+##' Returns the universe = totality of ID of an OrgDb annotation package
+##' 
+##' @title Returns the totality of ENTREZ ID (gene id) of an OrgDb annotation 
+##' package
+##' @param orgdb a Bioconductor OrgDb annotation package 
+##' @return A vector of ENTREZ ID (totality if the ID for the package) 
+##' @author Florence Combes
+##' 
+##' careful : org.Pf.plasmo.db : no 'ENTREZID' but 'ORF'
+#################################
+univ_AnnotDbPkg <- function(orgdb){
+    
+    require(as.character(orgdb),character.only = TRUE)
+    univ<-keys(get(orgdb), keytype="ENTREZID")
+    #different syntax for 'org.Pf.plasmo.db' package
+    #univ<-keys(get(orgdb), keytype="ORF")
+    return(univ)
+}
+
+
       
