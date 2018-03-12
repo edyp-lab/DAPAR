7039a54afbc0942967cb278e1c3e4cff1f6346d9
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2018-03-12 14:08:03 +0100

    update in pepa.test

--- R/pepa.R
+++ R/pepa.R
@@ -191,6 +191,111 @@ LH1 <- function(X, y1, y2, j){
 
 
 
+
+##' This function is xxxxxxx
+##' 
+##' @title xxxxxx
+##' @param X an n.pep*n.prot indicator matrix.
+##' @param y1 n.pep*n.samples matrice giving the observed counts for
+##' each peptide in each sample from the condition 1
+##' @param y2  n.pep*n.samples matrice giving the observed counts for
+##' each peptide in each sample from the condition 2 
+##' @return xxxxxxxxxx..
+##' @author Thomas Burger, Laurent Jacob
+LH0.lm <- function(X, y1, y2){
+    Ytilde <- matrix(c(as.vector(y1), as.vector(y2)), ncol=1)
+    p <- ncol(X)
+    q <- nrow(X)
+    n <- ncol(y1)+ncol(y2)
+    
+    Xcommon <- data.frame(do.call("rbind", rep(list(X), n)), row.names=as.character(1:(n*q)))
+    
+    ## Remove proteins containing all peptides (already represented as
+    ## the offset in the linear model)
+    rem.mask <- unlist(lapply(Xcommon, FUN=function(v) length(unique(v)) == 1))
+    
+    if(sum(rem.mask) == p){
+        if(q > 1){ # Only include a peptide effect if there is more than one peptide
+            Xpep <- factor(rep(rownames(X), n))
+            dataIn <- data.frame(Y=Ytilde, Xpep=Xpep, row.names=as.character(1:(n*q)))
+            lmm.res <- lm(Y ~ 1 + Xpep, data=dataIn)
+        }else{
+            dataIn <- data.frame(Y=Ytilde, row.names=as.character(1:(n*q)))
+            lmm.res <- lm(Y ~ 1, data=dataIn)
+        }
+    }else{
+        Xpep <- factor(rep(rownames(X), n))
+        Xcommon <- lapply(Xcommon[!rem.mask], factor)
+        dataIn <- data.frame(Y=Ytilde, Xcommon=Xcommon, Xpep=Xpep, row.names=as.character(1:(n*q)))
+        lmm.res <- lm(Y ~  ., data=dataIn)
+    }
+    ll <- logLik(lmm.res)
+    
+    return(list(ll=ll, lmm.res=lmm.res))
+}
+
+
+
+
+
+
+##' This function is xxxxxxx
+##' 
+##' @title xxxxxx
+##' @param X an n.pep*n.prot indicator matrix.
+##' @param y1 n.pep*n.samples matrix giving the observed counts for
+## each peptide in each sample from the condition 1
+##' @param y2  n.pep*n.samples matrix giving the observed counts for
+## each peptide in each sample from the condition 2 
+##' @param j the index of the protein being tested, ie which has different
+## expression in the two conditions under H1.
+##' @return xxxxxxxxxx..
+##' @author Thomas Burger, Laurent Jacob
+LH1.lm <- function(X, y1, y2, j){
+    n1 <- ncol(y1)
+    n2 <- ncol(y2)
+    n <- n1 + n2
+    p <- ncol(X)
+    q <- nrow(X)
+    xj <- X[, j, drop=FALSE]
+    Xj <- factor(c(rep(xj, n1), rep(0, n2*q)))    
+    Xpep <- factor(rep(rownames(X), n))
+    
+    Xcommon <- data.frame(do.call("rbind", rep(list(X), n)), row.names=as.character(1:(n*q)))
+    
+    ## Remove proteins containing all peptides (already represented as
+    ## the offset in the linear model)
+    rem.mask <- unlist(lapply(Xcommon, FUN=function(v) length(unique(v)) == 1))
+    
+    Ytilde <- matrix(c(as.vector(y1), as.vector(y2)), ncol=1)
+    
+    dataIn <- data.frame(Y=Ytilde, Xj=Xj, Xpep=Xpep, row.names=as.character(1:(n*q)))
+    
+    lmm.form <- 'Y ~'
+    if(ncol(X)>1 && sum(rem.mask) < p){
+        Xcommon <- lapply(Xcommon[!rem.mask], factor)
+        dataIn <- c(dataIn, Xcommon)
+        lmm.form <- paste(lmm.form, ' .-Xpep', sep='+', collapse='')
+    }else{
+        lmm.form <- paste(lmm.form, 'Xj', sep='+', collapse='')
+    }
+    if(q > 1){ # Only include a peptide effect if there is more than one peptide
+        lmm.form <- paste(lmm.form, ' Xpep', sep='+', collapse='') # Xpep effect
+        lmm.form <- formula(lmm.form)
+        lmm.res <- lm(lmm.form, data=dataIn)
+    }else{
+        lmm.form <- formula(lmm.form)
+        lmm.res <- lm(lmm.form, data=dataIn)
+    }
+    ll <- logLik(lmm.res)
+    
+    return(list(ll=ll, lmm.res=lmm.res))
+}
+
+
+
+
+
 ##' This function computes a regularized version of the likelihood ratio
 ##' statistic. The regularization adds a user-input fudge factor s1 to
 ##' the variance estimator. This is straightforward when using a fixed
@@ -283,25 +388,33 @@ samLRT <- function(lmm.res.h0, lmm.res.h1, cc, n, p, s1){
 ##' 
 ##' @title PEptide based Protein differential Abundance test
 ##' @param X Binary q x p design matrix for q peptides and p
-## proteins. X_(ij)=1 if peptide i belongs to protein j, 0 otherwise.
+##' proteins. X_(ij)=1 if peptide i belongs to protein j, 0 otherwise.
 ##' @param y q x n matrix representing the log intensities of q peptides
-## among n MS samples.
-##' @param n1  number of samples under condition 1. It is assumed that the first n1 columns of y
-## correspond to observations under condition 1. 
+##' among n MS samples.
+##' @param n1 number of samples under condition 1. It is assumed that the first n1 columns of y
+##' correspond to observations under condition 1.
 ##' @param n2 number of samples under condition 2.
+##' @param global if TRUE, the test statistic for each protein uses all
+##' residues, including the ones for peptides in different connected
+##' components. Can be much faster as it does not require to compute
+##' connected components. However the p-values are not well calibrated
+##' in this case, as it amounts to adding a ridge to the test
+##' statistic. Calibrating the p-value would require knowing the
+##' amplitude of the ridge, which in turns would require computing the
+##' connected components.
+##' @param use.lm if TRUE (and if global=FALSE), use lm() rather than the result
+##' in Proposition 1 to compute the test statistic
 ##' @return A list of the following elements:
-## llr: log likelihood ratio statistic (maximum likelihood version).
-## llr.map: log likelihood ratio statistic (maximum a posteriori version).
-## llr.pv: p-value for llr.
-## llr.map.pv: p-value for llr.map.
-## mse.h0: Mean squared error under H0
-## mse.h1: Mean squared error under H1
-## s: selected regularization hyperparameter for llr.map.
-## wchi2: weight used to make llr.map chi2-distributed under H0.
+##' llr: log likelihood ratio statistic (maximum likelihood version).
+##' llr.map: log likelihood ratio statistic (maximum a posteriori version).
+##' llr.pv: p-value for llr.
+##' llr.map.pv: p-value for llr.map.
+##' mse.h0: Mean squared error under H0
+##' mse.h1: Mean squared error under H1
+##' s: selected regularization hyperparameter for llr.map.
+##' wchi2: weight used to make llr.map chi2-distributed under H0.
 ##' @author Thomas Burger, Laurent Jacob
-
-
-pepa.test <- function(X, y, n1, n2){
+pepa.test <- function(X, y, n1, n2, global=FALSE, use.lm=FALSE){
     
     n <- n1+n2
     
@@ -315,66 +428,91 @@ pepa.test <- function(X, y, n1, n2){
     y1 <- y[, 1:n1]
     y2 <- y[, -(1:n1)]
     
-    ## Connected components: which peptides are connected to which proteins?
-    print('[pepa.test] Identifying connected components in the peptide-protein graph')
-    A <- matrix(0, nrow=p+q, ncol=p+q)
-    A[1:p, (p+1):(p+q)] <- as.matrix(t(X))
-    A[(p+1):(p+q), 1:p] <- as.matrix(X)
-    g <- graphAM(A, edgemode='undirected', values=NA)
-    nodes(g) <- as.character(1:(p+q))
-    cc <- connComp(as(g, 'graphNEL'))
-    
-    ## Test proteins in each connected component
-    protein.cc <- sapply(cc, FUN=function(ee){min(as.numeric(ee)) <= p})
-    cc <- cc[protein.cc]
-    
-    ## Compute 'local' likelihoods, using only peptides belonging to
-    ## protein of interest, or connected proteins.    
-    llr <-  llr.scalable <- lh1 <- lh0 <- n.pep <- rep(NA, p)
-    mse.h0 <- mse.h1 <- c()
-    ii <- 0
-    for(ee in cc){
-        ii <- ii + 1
-        print(sprintf('[pepa.test] Computing H0 likelihood for connected component %d/%d', ii, length(cc)))                
-        ee <- as.numeric(ee)
-        local.prot <- ee[ee <= p]
-        if(length(local.prot) == 0){next()}
-        local.pep <- ee[ee > p] - p
-        local.X <- X[local.pep, local.prot, drop=FALSE]
-        prot.barcode <- apply(local.X, 2, FUN=function(v) paste(as.character(v), collapse='-'))
-        bc.dup <- duplicated(prot.barcode)
-        equiv.X <- local.X[, !bc.dup, drop=FALSE]
-        
-        lik0 <- LH0(equiv.X, y1[local.pep, , drop=FALSE], y2[local.pep, , drop=FALSE])
-        mse.h0[ii] <- lik0$ss/(length(local.pep)*n)
-        
-        for(jj in local.prot){
+    if(global){
+        lik0 <- LH0(X, y1, y2)
+        mse.h0 <- lik0$ss/(q*n)
+        llr <- mse.h1 <- rep(NA, p)
+        for(jj in 1:p){
             print(sprintf('[pepa.test] Computing H1 likelihood for protein %d', jj))
-            prot.equiv.idx <- match(names(prot.barcode[!bc.dup])[prot.barcode[!bc.dup] == prot.barcode[colnames(X)[jj]]],
-                                    colnames(equiv.X))
-            lik1 <- LH1(equiv.X, y1[local.pep, , drop=FALSE], y2[local.pep, , drop=FALSE], prot.equiv.idx)            
-            mse.h1[jj] <- lik1$ss/(length(local.pep)*n)    
-            llr[jj] <- (length(local.pep)*n) * (log(lik0$ss) - log(lik1$ss))
+            lik1 <- LH1(X, y1, y2, jj)            
+            mse.h1[jj] <- lik1$ss/(q*n)    
+            llr[jj] <- (q*n) * (log(lik0$ss) - log(lik1$ss))
         }
+        ## We don't compute a regularized version of global-PEPA which is already a form of ridging
+        llr.map <- llr.map.pv <- s <- wchi2 <- NULL
+    }else{        
+        ## Connected components: which peptides are connected to which proteins?
+        print('[pepa.test] Identifying connected components in the peptide-protein graph')
+        A <- matrix(0, nrow=p+q, ncol=p+q)
+        A[1:p, (p+1):(p+q)] <- as.matrix(t(X))
+        A[(p+1):(p+q), 1:p] <- as.matrix(X)
+        g <- graphAM(A, edgemode='undirected', values=NA)
+        nodes(g) <- as.character(1:(p+q))
+        cc <- connComp(as(g, 'graphNEL'))
+        
+        ## Test proteins in each connected component
+        protein.cc <- sapply(cc, FUN=function(ee){min(as.numeric(ee)) <= p})
+        cc <- cc[protein.cc]
+        
+        ## Compute 'local' likelihoods, using only peptides belonging to
+        ## protein of interest, or connected proteins.    
+        llr <- lh1 <- lh0 <- n.pep <- rep(NA, p)
+        mse.h0 <- mse.h1 <- c()
+        ii <- 0
+        for(ee in cc){
+            ii <- ii + 1
+            print(sprintf('[pepa.test] Computing H0 likelihood for connected component %d/%d', ii, length(cc)))                
+            ee <- as.numeric(ee)
+            local.prot <- ee[ee <= p]
+            if(length(local.prot) == 0){next()}
+            local.pep <- ee[ee > p] - p
+            local.X <- X[local.pep, local.prot, drop=FALSE]
+            prot.barcode <- apply(local.X, 2, FUN=function(v) paste(as.character(v), collapse='-'))
+            bc.dup <- duplicated(prot.barcode)
+            equiv.X <- local.X[, !bc.dup, drop=FALSE]
+            
+            if(use.lm){
+                lik0 <- LH0.lm(equiv.X, y1[local.pep, , drop=FALSE], y2[local.pep, , drop=FALSE])
+                mse.h0[ii] <- mean(residuals(lik0$lmm.res)^2)
+            }else{
+                lik0 <- LH0(equiv.X, y1[local.pep, , drop=FALSE], y2[local.pep, , drop=FALSE])
+                mse.h0[ii] <- lik0$ss/(length(local.pep)*n)
+            }
+            
+            for(jj in local.prot){
+                print(sprintf('[pepa.test] Computing H1 likelihood for protein %d', jj))
+                prot.equiv.idx <- match(names(prot.barcode[!bc.dup])[prot.barcode[!bc.dup] == prot.barcode[colnames(X)[jj]]], colnames(equiv.X))
+                
+                if(use.lm){
+                    lik1 <- LH1.lm(equiv.X, y1[local.pep, , drop=FALSE], y2[local.pep, , drop=FALSE], prot.equiv.idx)
+                    mse.h1[jj] <- mean(residuals(lik1$lmm.res)^2)
+                    llr[jj] <- 2*(lik1$ll - lik0$ll)
+                }else{
+                    lik1 <- LH1(equiv.X, y1[local.pep, , drop=FALSE], y2[local.pep, , drop=FALSE], prot.equiv.idx)
+                    mse.h1[jj] <- lik1$ss/(length(local.pep)*n)    
+                    llr[jj] <- (length(local.pep)*n) * (log(lik0$ss) - log(lik1$ss))
+                }
+            }
+        }
+        s <- mse.h1   
+        if(p < 500){# Following SAM paper recommendations, we don't attempt to estimate s0 if p<500
+            s1 <- quantile(s, 0.05)
+        }else{
+            s1 <- fudge2LRT(mse.h0, mse.h1, cc, n, p, s)$s.zero
+        }
+        samLRT.res <- samLRT(mse.h0, mse.h1, cc, n, p, s1)
+        llr.map <- samLRT.res$llr.sam
+        ## Reweight LLR-sam for calibration of its p-value
+        s2.est <- mean(s)
+        wchi2 <- rep((s2.est + s1)/s2.est, length(de))
+        llr.map.pv <- 1 - pchisq(wchi2*llr.map, 1)
     }
-    s <- mse.h1   
-    if(p < 500){# Following SAM paper recommendations, we don't attempt to estimate s0 if p<500
-        s1 <- quantile(s, 0.05)
-    }else{
-        s1 <- fudge2LRT(mse.h0, mse.h1, cc, n, p, s)$s.zero
-    }
-    samLRT.res <- samLRT(mse.h0, mse.h1, cc, n, p, s1)
-    llr.map <- samLRT.res$llr.sam
     
-    ## Compute p-values
+    ## Compute p-value for llr
     llr.pv <- 1 - pchisq(llr, 1)
-    ## Reweight LLR-sam for calibration
-    s2.est <- mean(s)
-    wchi2 <- rep((s2.est + s1)/s2.est, length(de))
-    llr.map.pv <- 1 - pchisq(wchi2*llr.map, 1)
     
     return(list(llr=llr, llr.map=llr.map,
                 llr.pv=llr.pv, llr.map.pv=llr.map.pv,
                 mse.h0=mse.h0, mse.h1=mse.h1,
-                s=s, s1=s1, wchi2=wchi2))
+                s=s, wchi2=wchi2))
 }
