c47f562b5037511a79e123b246b45f65febadc8f
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2019-05-31 05:28:03 +0200

    Bug fixed in colors for MVplots in imputation

--- R/missingValuesPlots.R
+++ R/missingValuesPlots.R
@@ -693,8 +693,9 @@ wrapper.hc_mvTypePlot2 <- function(obj,...){
 ##' @title Distribution of Observed values with respect to intensity values
 ##' @param qData A dataframe that contains quantitative data.
 ##' @param conds A vector of the conditions (one condition per sample).
-##' @param palette xxx
+##' @param palette The different colors for conditions
 ##' @param typeofMV xxx
+##' @param title The title of the plot
 ##' @return Density plots
 ##' @author Samuel Wieczorek
 ##' @examples
@@ -702,107 +703,107 @@ wrapper.hc_mvTypePlot2 <- function(obj,...){
 ##' data(Exp1_R25_pept)
 ##' qData <- Biobase::exprs(Exp1_R25_pept)
 ##' conds <- Biobase::pData(Exp1_R25_pept)[,"Condition"]
-##' hc_mvTypePlot2(qData, conds)
-hc_mvTypePlot2 <- function(qData, conds, palette = NULL, typeofMV=NULL){
+##' hc_mvTypePlot2(qData, conds, title="POV distribution")
+hc_mvTypePlot2 <- function(qData, conds, palette = NULL, typeofMV=NULL, title=NULL){
   if (is.null(conds)){return(NULL)}
-    if (is.null(palette)){
-              palette <- brewer.pal(length(unique(conds)),"Dark2")[1:length(unique(conds))]
-    }else{
-      if (length(palette) != ncol(qData)){
-        warning("The color palette has not the same dimension as the number of samples")
-        return(NULL)
-      }
+  
+  if (is.null(palette)){
+    palette <- brewer.pal(length(unique(conds)),"Dark2")[1:length(unique(conds))]
+  }else{
+    if (length(palette) != length(unique(conds))){
+      warning("The color palette has not the same dimension as the number of conditions")
+      return(NULL)
     }
-  
-  
-  if (is.null(title)){
-    title <- "Missing values distribution"
-  } else {
-    title <- paste0(typeofMV, " values distribution")
   }
   
-    conditions <- conds
-    mTemp <- nbNA <- nbValues <- matrix(rep(0,nrow(qData)*length(unique(conditions))), nrow=nrow(qData),
-                                                     dimnames=list(NULL,unique(conditions)))
-    dataCond <- data.frame()
-    ymax <- 0
-    series <- list()
-    myColors <- NULL
-    j <- 1 
-    
-    for (iCond in unique(conditions)){
-        if (length(which(conditions==iCond)) == 1){
-           
-            mTemp[,iCond] <- qData[,which(conditions==iCond)]
-            nbNA[,iCond] <- as.integer(is.OfType(qData[,which(conditions==iCond)]))
-            nbValues[,iCond] <- length(which(conditions==iCond)) - nbNA[,iCond]
-        } else {
-            mTemp[,iCond] <- apply(qData[,which(conditions==iCond)], 1, mean, na.rm=TRUE)
-            nbNA[,iCond] <- apply(qData[,which(conditions==iCond)],1,function(x) length(which(is.na(x) == TRUE)))
-            nbValues[,iCond] <- length(which(conditions==iCond)) - nbNA[,iCond]
-        }
-        
-        
-        for (i in 1:length(which(conditions==iCond))){
-                data <- mTemp[which(nbValues[, iCond] == i), iCond]
-                tmp <- NULL    
-                    if (length(data) >= 2)
-                        {
-                        tmp <- density(mTemp[which(nbValues[,iCond]==i),iCond])
-                        tmp$y <- tmp$y + i
-                        if (max(tmp$y) > ymax) { ymax <- max(tmp$y)}
-                    }
-                        series[[j]] <- tmp
-                        myColors <- c(myColors, palette[which(unique(conditions)==iCond)])
-                j <- j+1
-            }
-
+  conditions <- conds
+  mTemp <- nbNA <- nbValues <- matrix(rep(0,nrow(qData)*length(unique(conditions))), nrow=nrow(qData),
+                                      dimnames=list(NULL,unique(conditions)))
+  dataCond <- data.frame()
+  ymax <- 0
+  series <- list()
+  myColors <- NULL
+  j <- 1 
+  
+  for (iCond in unique(conditions)){
+    if (length(which(conditions==iCond)) == 1){
+      
+      mTemp[,iCond] <- qData[,which(conditions==iCond)]
+      nbNA[,iCond] <- as.integer(is.OfType(qData[,which(conditions==iCond)]))
+      nbValues[,iCond] <- length(which(conditions==iCond)) - nbNA[,iCond]
+    } else {
+      mTemp[,iCond] <- apply(qData[,which(conditions==iCond)], 1, mean, na.rm=TRUE)
+      nbNA[,iCond] <- apply(qData[,which(conditions==iCond)],1,function(x) length(which(is.na(x) == TRUE)))
+      nbValues[,iCond] <- length(which(conditions==iCond)) - nbNA[,iCond]
     }
     
-
-    hc <-  highchart() %>%
-        hc_title(text = "POV distribution") %>%
-        my_hc_chart(chartType = "spline", zoomType="xy") %>%
-
-        hc_legend(align = "left", verticalAlign = "top",
-                  layout = "vertical") %>%
-        hc_xAxis(title = list(text = "Mean of intensities")) %>%
-        hc_yAxis(title = list(text = "Number ov values"),
-                 #categories = c(-1:3)
-                 #min = 1, 
-                # max = ymax,
-                 tickInterval= 0.5
-                 ) %>%
-        hc_colors(palette) %>%
-        hc_tooltip(headerFormat= '',
-                   pointFormat = "<b> {series.name} </b>: {point.y} ",
-                   valueDecimals = 2) %>%
-        my_hc_ExportMenu(filename = "POV_distribution") %>%
-        hc_plotOptions(
-            series=list(
-                showInLegend = TRUE,
-                animation=list(
-                    duration = 100
-                ),
-                connectNulls= TRUE,
-                marker=list(
-                    enabled = FALSE)
-                
-            )
-        )
     
-for (i in 1:length(series)){
-        hc <- hc_add_series(hc,data = list_parse(data.frame(cbind(x = series[[i]]$x, y = series[[i]]$y))), 
-                            showInLegend=FALSE, name=conds[i])
-}
+    for (i in 1:length(which(conditions==iCond))){
+      data <- mTemp[which(nbValues[, iCond] == i), iCond]
+      tmp <- NULL    
+      if (length(data) >= 2)
+      {
+        tmp <- density(mTemp[which(nbValues[,iCond]==i),iCond])
+        tmp$y <- tmp$y + i
+        if (max(tmp$y) > ymax) { ymax <- max(tmp$y)}
+      }
+      series[[j]] <- tmp
+      myColors <- c(myColors, palette[which(unique(conditions)==iCond)])
+      j <- j+1
+    }
     
-    # add three empty series for the legend entries. Change color and marker symbol
-for (c in 1:length(unique(conds))){
-      hc <-  hc_add_series(hc,data = data.frame(), name = unique(conds)[c], color = palette[c], marker = list(symbol = "circle"), type = "line")
-}
+  }
+  
+  
+  hc <-  highchart() %>%
+    hc_title(text = title) %>%
+    my_hc_chart(chartType = "spline", zoomType="xy") %>%
+    
+    hc_legend(align = "left", verticalAlign = "top",
+              layout = "vertical") %>%
+    hc_xAxis(title = list(text = "Mean of intensities")) %>%
+    hc_yAxis(title = list(text = "Number ov values"),
+             #categories = c(-1:3)
+             #min = 1, 
+             # max = ymax,
+             tickInterval= 0.5
+    ) %>%
+    # hc_colors(palette) %>%
+    hc_tooltip(headerFormat= '',
+               pointFormat = "<b> {series.name} </b>: {point.y} ",
+               valueDecimals = 2) %>%
+    my_hc_ExportMenu(filename = "POV_distribution") %>%
+    hc_plotOptions(
+      series=list(
+        showInLegend = TRUE,
+        animation=list(
+          duration = 100
+        ),
+        connectNulls= TRUE,
+        marker=list(
+          enabled = FALSE)
         
-hc
- return(hc)
+      )
+    )
+  
+  for (i in 1:length(series)){
+    hc <- hc_add_series(hc,
+                        data = list_parse(data.frame(cbind(x = series[[i]]$x, 
+                                                           y = series[[i]]$y))), 
+                        showInLegend=FALSE,
+                        color = myColors[i],
+                        name=conds[i])
+  }
+  
+  # add three empty series for the legend entries. Change color and marker symbol
+  for (c in 1:length(unique(conds))){
+    hc <-  hc_add_series(hc,data = data.frame(),
+                         name = unique(conds)[c],
+                         color = palette[c],
+                         marker = list(symbol = "circle"),
+                         type = "line")
+  }
+  
+  hc
+  return(hc)
 }
-
-
