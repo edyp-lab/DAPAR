09c033043dfcea55191557dabf75332ba74fe063
samWieczorek <samuel.wieczorek@cea.fr>
2020-03-22 06:59:22 +0100

    update in classes documentation

--- R/peptide-pipeline-class.R
+++ R/peptide-pipeline-class.R
@@ -1,21 +1,14 @@
 #' @import BiocGenerics MultiAssayExperiment S4Vectors methods
 
 
-#' @title PipelineTemplate class
-#' @description
-#' The \code{PipelineTemplate} class inherits from the \code{PipelineTemplate} and serves as a template for
-#' instanciate pipeline classes such as proteinPipeline, proteinPipeline, etc. It can be used to manage results of
-#' diverse assays on a collection of specimen. Currently,  the class can handle
-#' assays that are organized instances of
-#' \code{\linkS4class{MSnSet}},
-#' \code{matrix}.
+#' @title PipelinePeptide class
+#' @description xxxxx
 #'
 #'
-#' @slot analysis A character vector that is the name of the MS study
-#' @slot pipelineType A character vector that indicates the type of data that are managed in this instance
-#' @slot processes xxx
+#' @slot matAdj A character vector that is the name of the MS study
+#' @slot CC A character vector that indicates the type of data that are managed in this instance
 #'
-#' @return A \code{PipelineTemplate} object
+#' @return A \code{PipelinePeptide} object
 #'
 #' @examples
 #' example("PipelineTemp")
@@ -26,15 +19,6 @@
 #' mae <- PipelinePeptide(analysis= 'test',pipelineType = 'peptide', dataType = 'peptide',
 #' processes='original',experiments=list(original=obj), 
 #' colData=samples)
-
-
-#' @title sdfdsfs
-#' @description sfklsjhf qsjdhsqd.
-#' @param obj xxxx
-#' @return xxxx
-#' @author Samuel Wieczorek
-#' @examples 
-#' utils::data(Exp1_R25_prot)
 #' @export
 #' @exportClass PipelinePeptide
 .PipelinePeptide <- setClass("PipelinePeptide",
--- R/protein-pipeline-class.R
+++ R/protein-pipeline-class.R
@@ -1,25 +1,13 @@
 #' @import BiocGenerics MultiAssayExperiment S4Vectors methods
 
 
-#' @title PipelineTemplate class
-#' @description
-#' The \code{PipelineTemplate} class inherits from the \code{PipelineTemplate} and serves as a template for
-#' instanciate pipeline classes such as proteinPipeline, proteinPipeline, etc. It can be used to manage results of
-#' diverse assays on a collection of specimen. Currently,  the class can handle
-#' assays that are organized instances of
-#' \code{\linkS4class{MSnSet}},
-#' \code{matrix}.
+#' @title PipelineProtein class
+#' @description xx x 
 #'
-#'
-#' @slot PairwiseComparisons A \code{list} to store the result of hypothesis tests.
-#' @slot indexNA A xxxxxs
-#' @slot analysis A character vector that is the name of the MS study
-#' @slot pipelineType A character vector that indicates the type of data that are managed in this instance
-#' @slot processes xxx
 #'
 #' @param ... Additional arguments for supporting functions. See details.
 #'
-#' @return A \code{PipelineTemplate} object
+#' @return A \code{PipelineProtein} object
 #'
 #' @examples
 #' example("PipelineTemp")
@@ -29,8 +17,6 @@
 #' mae <- PipelineProtein(analysis= 'test',pipelineType = 'protein', dataType = 'protein',
 #' processes='original',experiments=list(original=Exp1_R25_prot), 
 #' colData=samples)
-
-
 #' @exportClass PipelineProtein
 .PipelineProtein <- setClass("PipelineProtein",
           contains = "PipelineTemplate", 
