6db03824a694a4d47f900eec092f1c063037903f
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2020-09-22 09:58:55 +0200

    fix bug in anova example

--- R/anova_analysis.R
+++ R/anova_analysis.R
@@ -15,7 +15,7 @@
 #' obj <- Exp1_R25_prot[1:1000]
 #' keepThat <- mvFilterGetIndices(obj, 'wholeMatrix', ncol(obj))
 #' obj <- mvFilterFromIndices(obj, keepThat)
-#' anova_tests <- t(apply(exprs(obj),1, classic1wayAnova, conditions=as.factor(pData(obj)$Condition)))
+#' anova_tests <- t(apply(Biobase::exprs(obj),1, classic1wayAnova, conditions=as.factor(Biobase::pData(obj)$Condition)))
 #' 
 #' @importFrom stats aov
 #' 
@@ -110,7 +110,7 @@ wrapperClassic1wayAnova <- function(obj, with_post_hoc = "No", post_hoc_test = "
 #' obj <- Exp1_R25_prot[1:1000]
 #' keepThat <- mvFilterGetIndices(obj, 'wholeMatrix', ncol(obj))
 #' obj <- mvFilterFromIndices(obj, keepThat)
-#' anova_tests <- t(apply(exprs(obj),1, classic1wayAnova, conditions=as.factor(pData(obj)$Condition)))
+#' anova_tests <- t(apply(Biobase::exprs(obj),1, classic1wayAnova, conditions=as.factor(Biobase::pData(obj)$Condition)))
 #' names(anova_tests) <- rownames(exprs(obj))
 #' tms <- lapply(anova_tests,
 #'              function(x) summary(multcomp::glht(x, linfct = multcomp::mcp(conditions = "Tukey")),
@@ -186,7 +186,7 @@ formatPHResults <- function(post_hoc_models_summaries){
 #' obj <- Exp1_R25_prot[1:1000]
 #' keepThat <- mvFilterGetIndices(obj, 'wholeMatrix', ncol(obj))
 #' obj <- mvFilterFromIndices(obj, keepThat)
-#' anova_tests <- t(apply(exprs(obj),1, classic1wayAnova, conditions=as.factor(pData(obj)$Condition)))
+#' anova_tests <- t(apply(Biobase::exprs(obj),1, classic1wayAnova, conditions=as.factor(Biobase::pData(obj)$Condition)))
 #' names(anova_tests) <- rownames(exprs(obj))
 #' pht <- postHocTest(aov_fits = anova_tests)
 #' 
