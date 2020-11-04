3f2e154a2c00a9325835f4b94d209dcd52046218
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2020-11-04 10:45:37 +0100

    Use crossprod instead of %&% in compte CC

--- R/get_pep_prot_cc.R
+++ R/get_pep_prot_cc.R
@@ -13,7 +13,7 @@
 #' X <- BuildAdjacencyMatrix(Exp1_R25_pept[1:1000], "Protein_group_IDs", FALSE)
 #' ll <- get.pep.prot.cc(X)
 #' 
-#' @importFrom Matrix %&%
+#' @importFrom Matrix %&% crossprod
 #' @importFrom graph graphAM connComp 
 #' 
 #' @export
@@ -32,7 +32,8 @@ get.pep.prot.cc <- function(X){
   A <- B <- g <- NULL
   ### Adjacency matrix construction
   # boolean matrix product
-  A <- as.matrix(t(X) %&% X) 
+  #A <- as.matrix(t(X) %&% X) 
+  A <- Matrix::crossprod(X, boolArith=TRUE)
   # remove self-connecting edges
   diag(A) <- rep(0,p)
   A <- matrix(as.numeric(A[,]), ncol=p) # goes back to classical matrix format
