c966964f8c1b3f2878053f62de9be8d34386dc5d
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2023-12-21 15:23:34 +0100

    test

---
+++ R/dataset_Validity.R
@@ -0,0 +1,12 @@
+
+
+
+#' @title xxx
+#' @description xxx
+#' @param obj xxx
+#' @export
+Check_Dataset_Validity <- function(obj){
+  is.valid <- TRUE
+  is.valid <- !is.null(obj)
+  return(is.valid)
+}
